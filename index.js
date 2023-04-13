2f69140271c1d05af7c798447c07d09a669e1c50
Matadorz350 <kljatva1983@gmail.com>
2023-04-13 16:25:37 +0500

    I've added extra functions

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const charactersSym = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+  "/"];
 
 let passwordOne = document.getElementById("password-one");
 let passwordTwo = document.getElementById("password-two");
+let amountEl = document.getElementById("amount-el");
+let x = 12;
+
+function includSym() {
+  characters.push(...charactersSym);
+}
+
+function changeNum() {
+  x = amountEl.value;
+}
 
 function generator() {
   let stringOne = "";
   let stringTwo = "";
-  for(let i = 0; i < 15; i++) {
+  for(let i = 0; i < x; i++) {
     let randomNumber = Math.floor(Math.random() * characters.length);
     let randomNumberTwo = Math.floor(Math.random() * characters.length);
     stringOne += characters[randomNumber];
